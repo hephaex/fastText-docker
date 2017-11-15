5bdec1d14b5bf0598f10b8f3d4709e52da4b7e18
Mario Cho <hephaex@gmail.com>
2017-11-15 14:34:06 +0900

    modify train script

--- example-train.sh
+++ example-train.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-./fasttext supervised -input /data/cooking.train -output /data/model_cooking
+./fasttext supervised -input {$1}/{$2}.train \
+    -output "{$3}/{$4}" -dim 10 -lr 0.5 -wordNgrams 2 \
+    -minCount 1 -bucket 10000000 -epoch 5 -thread 4 > /dev/null
+  ./fasttext test "{$3}/{$4}.bin" \
+             "{$1}/{$2}.test"
 
+#./fasttext supervised -input /data/cooking.train -output /data/model_cooking
